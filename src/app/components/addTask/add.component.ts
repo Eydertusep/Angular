279b8767215a1688f8098b0f4ec1889f201fb667
[Eyder] <[eyder.alexander.riwi@gmail.com]>
2025-02-07 18:20:49 -0500

    Angular.1

---
+++ src/app/components/addTask/add.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: `app-addTask`, /* Siempre desde de empezar por "app-"" */
+  templateUrl: `./add.component.html`, /* Le añadimos la vista del componente */  
+  styleUrl: `./add.component.css` /* Le añadimos el css */
+})
+
+/* Los componentes en angular son una clase que va a ser exportada, para llamarlos en los modulos */
+
+export class AddComponent {
+
+}
