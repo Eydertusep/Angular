5e19e9a496159d79b7c98a4c469a64ec4c7ca0f2
[Eyder] <[eyder.alexander.riwi@gmail.com]>
2025-02-07 23:07:50 -0500

    pequeño error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { clearInterval } from 'node:timers';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,26 @@ import { Component } from '@angular/core';
   standalone: false,
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'proyect-angular';
+export class AppComponent  {
+  /*ngAfterViewInit(): void {
+      console.log("han sido inicializadas la vista de los componentes y la vista de lo hijos");
+      
+  }*/
+  
+  isDestroyed: boolean = true;
+
+  countDown: number;
+  intervalID: any; // Any = El tipo puede ser de cualquier cosa
+
+  constructor() {
+    this.countDown = 10; 
+    this.intervalID = setInterval(() => {
+      this.countDown--;
+      if (this.countDown === 0) {
+        clearInterval(this.intervalID)
+        this.isDestroyed = false
+      }
+    }, 100000)
+  }
+  
 }
--- src/app/components/addTask/add.component.ts
+++ src/app/components/addTask/add.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
 
 @Component({
   selector: `app-addTask`, /* Siempre desde de empezar por "app-"" */
@@ -8,8 +8,21 @@ import { Component } from "@angular/core";
 
 /* Los componentes en angular son una clase que va a ser exportada, para llamarlos en los modulos */
 
-export class AddComponent {
+export class AddComponent implements OnDestroy{
 
+  /* constructor() {
+    console.log("Creandose desde el constructor");
+    
+  }
+  ngOnInit(): void {
+      console.log("Creandose desde el ngOninit");
+      
+  } /* Se ejecuta cuando el componente se haya creado */
+  
+  ngOnDestroy(): void {
+      console.log("El componente dd ha sido destruido");
+      
+  }
 }
 
 
